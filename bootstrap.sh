99c6978ab95fbb0be816f285d3e2fba3788604f4
Robin Gloster <robin@loc-com.de>
2014-05-29 23:13:35 +0200

    Use devstack.yaml for Vagrantfile configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,6 @@ readonly DIR=$(readlink -m $(dirname $0))
 main() {
     echo "[bootstrap] link Vagrantfile" && \
         ln -si $DIR/_Vagrantfile $DIR/../Vagrantfile
-    echo "[bootstrap] create vagrant_project_config" && \
-        cp -i $DIR/config.dist $DIR/../vagrant_project_config
     echo "[bootstrap] create devstack.yaml" && \
         cp -i $DIR/devstack.yaml.dist $DIR/../devstack.yaml
 }
