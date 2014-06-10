878c3de126802497e95873fbe551b02afe3a7192
Robin Gloster <robin@loc-com.de>
2014-06-11 00:24:51 +0200

    add less

--- manifests/ubuntu_devstack.pp
+++ manifests/ubuntu_devstack.pp
@@ -27,6 +27,11 @@ class ubuntu_devstack {
     Class['component::symfony2']
   }
 
+  if hiera('less', false) {
+    class { 'component::less': } ->
+    Class['component::symfony2']
+  }
+
   # We need this dependency handling because otherwise we get a dependency cycle with Service['nginx'] and apt
   class { 'nginx': }
 }
---
+++ site/component/manifests/less.pp
@@ -0,0 +1,5 @@
+class component::less {
+  package { 'less':
+    provider => npm
+  }
+}
