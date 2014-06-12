1c4f4ae439c5c6e1da82c803eb6061c777d5eab9
Robin Gloster <robin@loc-com.de>
2014-06-12 19:47:38 +0200

    add redis

--- site/profile/manifests/database.pp
+++ site/profile/manifests/database.pp
@@ -1,9 +1,11 @@
 class profile::database (
   $mysql      = true,
-  $postgresql = false
+  $postgresql = false,
+  $redis      = false,
 ) {
   validate_bool($mysql)
   validate_bool($postgresql)
+  validate_bool($redis)
 
   anchor { 'profile::database::begin': } ->
   anchor { 'profile::database::end': }
@@ -17,6 +19,12 @@ class profile::database (
   if $postgresql {
     Anchor['profile::database::begin'] ->
     class { 'component::postgresql': } ->
+    Anchor['profile::database::end']
+  }
+
+  if $redis {
+    Anchor['profile::database::begin'] ->
+    class { 'component::redis': } ->
     Anchor['profile::database::end']
   }
 }
