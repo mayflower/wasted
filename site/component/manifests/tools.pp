c58d523c60482772bb15650d7bd2efd01f12f198
Robin Gloster <robin@loc-com.de>
2014-06-20 14:28:41 +0200

    double package declaration

--- site/component/manifests/tools.pp
+++ site/component/manifests/tools.pp
@@ -1,7 +1,6 @@
 class component::tools {
     package { 'vim-nox': }
     package { 'htop': }
-    package { 'htop': }
     package { 'bash-completion': }
     package { 'wget': }
     package { 'curl': }
