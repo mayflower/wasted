2451963e6188a4cbcf30e1741ae331e561b969bc
Robin Gloster <robin@loc-com.de>
2014-06-20 10:15:33 +0200

    fixes #19 link of Vagrantfile was absolute

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,9 @@ readonly DIR=$(readlink -m $(dirname $0))
 
 main() {
     echo "[bootstrap] link Vagrantfile to project root" && \
-        ln -si $DIR/Vagrantfile $DIR/../Vagrantfile
+        ln -si vagrant/Vagrantfile $DIR/../Vagrantfile
     echo "[bootstrap] link .vagrant to project root" && \
-        ln -si $DIR/.vagrant $DIR/../.vagrant
+        ln -si vagrant/.vagrant $DIR/../.vagrant
     echo "[bootstrap] create devstack.yaml in project root" && \
         cp -i $DIR/devstack.yaml.dist $DIR/../devstack.yaml
 }
