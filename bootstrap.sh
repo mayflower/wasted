c1d0ccb12848c0a46443a167ac08bf55b3f4f803
Franz Pletz <fpletz@fnordicwalking.de>
2014-07-03 13:48:32 +0200

    bootstrap.sh: Create vagrant/.vagrant if needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,8 @@ main() {
     echo "[bootstrap] link Vagrantfile to project root" && \
         ln -si vagrant/Vagrantfile $DIR/../Vagrantfile
     echo "[bootstrap] link .vagrant to project root" && \
-        ln -si vagrant/.vagrant $DIR/../.vagrant
+        ln -si vagrant/.vagrant $DIR/../.vagrant &&
+        (mkdir vagrant/.vagrant 2>/dev/null || true)
     echo "[bootstrap] create devstack.yaml in project root" && \
         cp -i $DIR/devstack.yaml.dist $DIR/../devstack.yaml
 }
