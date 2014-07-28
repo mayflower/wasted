08e1a4e10eec437caf03b8bd2f05849344aef27d
Franz Pletz <fpletz@fnordicwalking.de>
2014-07-28 11:28:28 +0200

    Support custom nginx vhosts and locations

--- site/component/manifests/nginx.pp
+++ site/component/manifests/nginx.pp
@@ -1,5 +1,15 @@
-class component::nginx {
+class component::nginx (
+  $vhosts = {},
+  $locations = {},
+) {
+
+  validate_hash($vhosts)
+  validate_hash($locations)
+
   anchor { 'component::nginx::begin': } ->
     class { '::nginx': } ->
   anchor { 'component::nginx::end': }
+
+  create_resources('::nginx::resource::vhost', $vhosts)
+  create_resources('::nginx::resource::location', $locations)
 }
