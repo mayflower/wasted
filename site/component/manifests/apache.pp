4e17c0a40e078fb7078d4ada01392b361988f053
Franz Pletz <fpletz@fnordicwalking.de>
2014-07-28 12:12:18 +0200

    Add support for loading custom apache modules

--- site/component/manifests/apache.pp
+++ site/component/manifests/apache.pp
@@ -1,16 +1,20 @@
 class component::apache (
   $php = false,
   $vhosts = {},
+  $mods = [],
+  $custom_mods = {},
 ) {
 
   validate_bool($php)
   validate_hash($vhosts)
+  validate_array($mods)
+  validate_hash($custom_mods)
 
   anchor { 'component::apache::begin': } ->
     class { '::apache':
       default_vhost => false,
     } ->
-  anchor { 'component::apche::end': }
+  anchor { 'component::apache::end': }
 
   if $php {
     ::apache::mod { 'proxy': }
@@ -18,4 +22,13 @@ class component::apache (
   }
 
   create_resources('::apache::vhost', $vhosts)
+
+  # Mods for which a custom class with the prefix apache::mod exists
+  $mods_classes = regsubst($mods, '^(.+)$', '::apache::mod::\1')
+  Anchor['component::apache::begin'] ->
+    class { $mods_classes: } ->
+  Anchor['component::apache::end']
+
+  # All other mods are defined resources
+  create_resources('::apache::mod', $custom_mods)
 }
