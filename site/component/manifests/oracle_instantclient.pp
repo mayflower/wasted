c84d84318b6fd70e369c2137d83047f642f79513
Robin Gloster <robin@loc-com.de>
2014-09-14 16:02:03 +0200

    fix dependency cycle

--- site/component/manifests/oracle_instantclient.pp
+++ site/component/manifests/oracle_instantclient.pp
@@ -1,13 +1,6 @@
 class component::oracle_instantclient {
-
-  apt::source { 'mayflower-obs-oracle-instantclient':
-    location    => 'http://download.opensuse.org/repositories/home:/mayflower/xUbuntu_14.04/',
-    release     => './',
-    repos       => '',
-    key         => '9CA70524',
-    key_source  => 'http://download.opensuse.org/repositories/home:/mayflower/xUbuntu_14.04/Release.key',
-    include_src => false,
-  } ->
+  # don't contain the repository to avoid a dependency cycle
+  include apt
 
   package { 'oracle-instantclient':
     ensure => present,
