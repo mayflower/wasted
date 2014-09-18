a8b593b3d88da8947404a5465ef7b88f0ca4228e
Franz Pletz <fpletz@fnordicwalking.de>
2014-09-18 13:39:52 +0200

    Deprecate setting component::mysql::root_password

--- site/component/manifests/mysql.pp
+++ site/component/manifests/mysql.pp
@@ -1,16 +1,14 @@
 class component::mysql (
-  $root_password = 'root',
+  $root_password = undef,
   $databases = {}
 ) {
-  validate_string($root_password)
   validate_hash($databases)
 
-  anchor { 'component::mysql::begin': } ->
-    class {'::mysql::server':
-      root_password => $root_password
-    } ->
-  anchor { 'component::mysql::end': }
+  if $root_password != undef {
+    fail('Setting the MySQL root password with component::mysql::root_password is not supported anymore. Please use mysql::server::root_password instead!')
+  }
 
+  contain '::mysql::server'
   contain '::mysql::client'
 
   create_resources('::mysql::db', $databases)
