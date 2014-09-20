4de230fdc0a05a114557fdd72ffa08e14aa3af88
Robin Gloster <robin@loc-com.de>
2014-09-20 12:01:03 +0200

    fix deep_merge installation

--- puppet-bootstrap.sh
+++ puppet-bootstrap.sh
@@ -2,12 +2,13 @@
 
 cd /vagrant/vagrant
 
+apt-get update -qq
+[ -x /usr/bin/gem ] || apt-get install -y -q rubygems
+gem list | grep deep_merge > /dev/null || gem install --no-rdoc --no-ri deep_merge
+
 if [ ! -e modules/.r10k_stamp ] || [ modules/.r10k_stamp -ot Puppetfile ]; then
-  apt-get update -qq
   [ -x /usr/bin/git ] || apt-get install -y -q git
-  [ -x /usr/bin/gem ] || apt-get install -y -q rubygems
   [ -x /usr/local/bin/r10k ] || gem install --no-rdoc --no-ri r10k
-  gem list | grep deep_merge > /dev/null || gem install --no-rdoc --no-ri deep_merge
 
   r10k -v info puppetfile install
   touch modules/.r10k_stamp
