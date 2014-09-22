7007ce3d5ce19ae8ef1912851997f6fa3ffbc6f0
Simon Waibl <elseym@me.com>
2014-09-22 11:15:51 +0200

    remove obsolete default.pp

--- site/profile/manifests/default.pp
+++
@@ -1,6 +0,0 @@
-class profile::default {
-  anchor { 'profile::default::begin': } ->
-    class { 'component::sync': } ->
-    class { 'component::tools': } ->
-  anchor { 'profile::default::end': }
-}
