24871048e69e72ab2caadbebfb8f0be888364aa6
Robin Gloster <robin@loc-com.de>
2014-12-03 12:45:33 +0100

    puppet module needs tar, was not in path

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -11,7 +11,7 @@ node default {
   } ->
 
   exec { 'r10k-puppetfile-install':
-    path    => '/usr/bin:/usr/local/bin',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     command => 'r10k -v info puppetfile install && touch modules/.r10k_stamp',
     cwd     => '/vagrant/vagrant',
     onlyif  => 'test ! -e modules/.r10k_stamp || test modules/.r10k_stamp -ot Puppetfile',
