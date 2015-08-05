70730621b148230fc0c35e38090028fc6ea6de1f
Robin Gloster <robin@loc-com.de>
2015-08-05 11:04:57 +0000

    fix failure of resources not existing

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -8,18 +8,8 @@ node default {
     exec { 'apt-update':
       command => 'apt-get update',
       unless  => 'which git',
+      before => Package['ruby', 'git', 'r10k']
     } ->
-
-    package { ['ruby', 'git']:
-      ensure        => installed,
-      allow_virtual => true,
-    } ->
-
-    package { ['deep_merge', 'r10k']:
-      ensure   => installed,
-      provider => 'gem',
-    } ->
-
     file { ['/etc/facter', '/etc/facter/facts.d/']:
       ensure  => directory,
       recurse => true,
@@ -37,10 +27,19 @@ node default {
     }
   }
 
+  package { ['ruby', 'git']:
+    ensure        => installed,
+    allow_virtual => true,
+  } ->
+
+  package { ['deep_merge', 'r10k']:
+    ensure   => installed,
+    provider => 'gem',
+  } ->
+
   exec { 'r10k-puppetfile-install':
     command => 'r10k -v info puppetfile install && touch modules/.r10k_stamp',
     cwd     => '/vagrant/vagrant',
     onlyif  => 'test ! -e modules/.r10k_stamp || test modules/.r10k_stamp -ot Puppetfile',
-    require => Package['ruby', 'git', 'r10k']
   }
 }
