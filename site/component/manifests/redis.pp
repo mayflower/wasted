1e738fa4d1b55df47075160b1f0061324034f603
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2015-10-06 21:58:41 +0200

    refactored redis component
    - use third party module for redis
    - make multiple redis instances configurable

--- site/component/manifests/redis.pp
+++ site/component/manifests/redis.pp
@@ -1,3 +1,6 @@
-class component::redis {
-  package { 'redis-server': }
+class component::redis($instances = {}) {
+  validate_hash($instances)
+
+  contain '::redis'
+  create_resources('::redis::instance', $instances)
 }
