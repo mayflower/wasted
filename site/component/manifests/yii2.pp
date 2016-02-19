ff706feec61c5c21c0d13c466ed6fb30d1478321
Florian Fackler <florian.fackler@mintao.com>
2016-02-19 21:05:53 +0100

    Extend configuration for yii2

--- site/component/manifests/yii2.pp
+++ site/component/manifests/yii2.pp
@@ -1,25 +1,82 @@
 class component::yii2 (
-  $path  = hiera('path', '/var/www/app_name'),
-  $vhost = hiera('vhost', 'app-name.dev'),
-  $env   = hiera('env', 'dev'),
+  $path             = hiera('path', '/var/www/app_name'),
+  $vhost            = hiera('vhost', 'app-name.dev'),
+  $vhost_port       = 80,
+  $env              = hiera('env', 'dev'),
+  $front_controller = undef,
 ) {
 
-  nginx::resource::vhost { $vhost:
-    www_root            => "${path}/web",
-    fastcgi             => '127.0.0.1:9000',
-    location_cfg_append => {
-      fastcgi_index => 'index.php',
-      fastcgi_param => [
-        'SCRIPT_FILENAME $document_root/index.php',
-        "APPLICATION_ENV ${env}"
-      ]
+  $entrypoint = $front_controller ? {
+    undef => $env ? {
+      /test/  => 'index-test.php',
+      default => 'app.php'
     },
+    default => $front_controller
   }
 
-  nginx::resource::location{ "${vhost}_static":
-    location  => '~ ^/(assets|css|images|js)/',
-    vhost     => $vhost,
-    www_root  => "${path}/web",
-    try_files => ['$uri', '=404']
+  $location_index = regsubst($entrypoint, '\.', '\.')
+
+  nginx::resource::vhost { "${vhost}-${vhost_port}-yii2":
+    server_name => [$vhost],
+    listen_port => $vhost_port,
+    www_root    => "${path}/web",
+    index_files => [$entrypoint],
+    try_files   => ['$uri', '@rewriteapp'],
+  }
+
+  nginx::resource::location { '@rewriteapp':
+    vhost         => "${vhost}-${vhost_port}-yii2",
+    www_root      => "${path}/web",
+    rewrite_rules => ["^(.*)\$ /${entrypoint}/\$1 last"]
+  }
+
+  nginx::resource::location { "~ ^/${location_index}(/|\$)":
+    vhost               => "${vhost}-${vhost_port}-yii2",
+    www_root            => "${path}/web",
+    fastcgi             => '127.0.0.1:9000',
+    fastcgi_split_path  => '^(.+\.php)(/.+)$',
+    location_cfg_append => {
+      fastcgi_buffer_size       => '128k',
+      fastcgi_buffers           => '4 256k',
+      fastcgi_busy_buffers_size => '256k',
+      fastcgi_param => [
+        'SCRIPT_FILENAME $document_root/index.php',
+        "YII_ENV ${env}"
+      ]
+    }
+  }
+
+  if defined(Class['::hhvm']) {
+    nginx::resource::vhost { "hhvm.${vhost}-${vhost_port}-yii2":
+      server_name => ["hhvm.${vhost}"],
+      listen_port => $vhost_port,
+      www_root    => "${path}/web",
+      index_files => [$entrypoint],
+      try_files   => ['$uri', '@rewriteapp'],
+    }
+
+    nginx::resource::location { 'hhvm-sf2-rewrite':
+      location      => '@rewriteapp',
+      vhost         => "hhvm.${vhost}-${vhost_port}-yii2",
+      www_root      => "${path}/web",
+      rewrite_rules => ["^(.*)\$ /${entrypoint}/\$1 last"]
+    }
+
+    nginx::resource::location { 'hhvm-sf2-php':
+      location            => "~ ^/${location_index}(/|\$)",
+      vhost               => "hhvm.${vhost}-${vhost_port}-yii2",
+      www_root            => "${path}/web",
+      fastcgi             => '127.0.0.1:9090',
+      fastcgi_split_path  => '^(.+\.php)(/.+)$',
+      location_cfg_append => {
+        fastcgi_buffer_size       => '128k',
+        fastcgi_buffers           => '4 256k',
+        fastcgi_busy_buffers_size => '256k',
+        fastcgi_param => [
+          'SCRIPT_FILENAME $document_root/index.php',
+          "YII_ENV ${env}"
+        ]
+      }
+    }
   }
 }
